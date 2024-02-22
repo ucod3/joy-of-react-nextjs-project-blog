fef8ab63d04c38c2ea826559a33d829ae5ba97c8
Usman Butt <usman.butt@outlook.com>
2024-02-22 17:26:57 -0500

    Add RespectMotionPreferences component to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import { LIGHT_TOKENS, DARK_TOKENS, BLOG_TITLE } from '@/constants';
 
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import RespectMotionPreferences from '@/components/RespectMotionPreferences';
 import './styles.css';
 
 export const metadata = {
@@ -31,18 +32,20 @@ function RootLayout({ children }) {
   const theme = 'light';
 
   return (
-    <html
-      lang='en'
-      className={clsx(mainFont.variable, monoFont.variable)}
-      data-color-theme={theme}
-      style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
-    >
-      <body>
-        <Header theme={theme} />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <RespectMotionPreferences>
+      <html
+        lang='en'
+        className={clsx(mainFont.variable, monoFont.variable)}
+        data-color-theme={theme}
+        style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
+      >
+        <body>
+          <Header theme={theme} />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </RespectMotionPreferences>
   );
 }
 
