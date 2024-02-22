f0161201b1ca8e44adba17afde8f970afc4e3710
Usman Butt <usman.butt@outlook.com>
2024-02-22 13:50:36 -0500

    Add BLOG_TITLE to page title

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import BlogHero from '@/components/BlogHero';
+import { BLOG_TITLE } from '@/constants';
 
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import styles from './postSlug.module.css';
@@ -9,7 +10,7 @@ export async function generateMetadata({ params }) {
   const { frontmatter } = await loadBlogPost(params.postSlug);
 
   return {
-    title: frontmatter.title,
+    title: `${frontmatter.title} | ${BLOG_TITLE}`,
     description: frontmatter.abstract,
   };
 }
