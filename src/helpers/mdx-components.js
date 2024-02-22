d86d9024ee52ff79e81558bd46a436bdd69f55d7
Usman Butt <usman.butt@outlook.com>
2024-02-22 15:58:23 -0500

    Add new MDX components

---
+++ src/helpers/mdx-components.js
@@ -0,0 +1,8 @@
+import CodeSnippet from '@/components/CodeSnippet';
+import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
+
+const COMPONENTS_MAP = {
+  pre: CodeSnippet,
+  DivisionGroupsDemo,
+};
+export default COMPONENTS_MAP;
