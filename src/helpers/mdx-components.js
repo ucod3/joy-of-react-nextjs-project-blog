181251db21878fb1918b2ac77299b4faf8ef9dbd
Usman Butt <usman.butt@outlook.com>
2024-02-22 20:33:13 -0500

    Add CircularColorsDemo component to mdx-components.js

--- src/helpers/mdx-components.js
+++ src/helpers/mdx-components.js
@@ -1,8 +1,10 @@
 import CodeSnippet from '@/components/CodeSnippet';
 import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
+import CircularColorsDemo from '@/components/CircularColorsDemo';
 
 const COMPONENTS_MAP = {
   pre: CodeSnippet,
   DivisionGroupsDemo,
+  CircularColorsDemo,
 };
 export default COMPONENTS_MAP;
